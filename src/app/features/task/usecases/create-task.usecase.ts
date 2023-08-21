b0b96be4de679825e0785f8360f8a7511cf7aaab
Assferj <assisjuniorcam@gmail.com>
2023-08-21 12:21:01 -0300

    feat: add cache to dreate task

--- src/app/features/task/usecases/create-task.usecase.ts
+++ src/app/features/task/usecases/create-task.usecase.ts
@@ -1,9 +1,18 @@
 import { Task } from "../../../models/Task";
+import { CacheRepository } from "../../../shared/database/repository/cache.repositiry";
 import { TaskRepository } from "../repository/task.repository";
 
 export class CreateTaskUsecase {
     public async execute (task: Task) {
         const repository = new TaskRepository();
         const result = repository.addTask(task)
+        const cacheRepository = new CacheRepository();
+        await cacheRepository.delete("tasks");
+        return {
+            ok: true,
+            message: "Task successfully created",
+            code: 200,
+            data: result
+        };
     }
 }
