b84dd8b2876a64bd42801fdbd055e5e12d6b9d8a
AssFerj <assisjuniorcam@gmail.com>
2023-08-20 21:44:17 -0300

    feat: added cache to list tasks and get task by id

--- src/app/features/task/usecases/get-task-by-id.usecase.ts
+++ src/app/features/task/usecases/get-task-by-id.usecase.ts
@@ -1,14 +1,25 @@
+import { CacheRepository } from "../../../shared/database/repository/cache.repositiry";
 import { TaskRepository } from "../repository/task.repository";
 
 export class GetTaskByIdUsecase {
     public async execute(taskId: string) {
         const repository = new TaskRepository()
+        const cacheRepository= new CacheRepository()
+        const cache = await cacheRepository.get(`tasks-${taskId}`)
+        if(cache){
+            return {
+                ok: true,
+                code: 200,
+                message: 'Tasks successfully listed in cache',
+                date: cache
+            }
+        }
         const result = await repository.getTaskById(taskId)
         
         if(!result) {
             return `Task not found`
         }
-
+        await cacheRepository.set(`tasks-${taskId}`, result.toJson())        
         return result.toJson();
         
     }
--- src/app/features/task/usecases/list-tasks.usecase.ts
+++ src/app/features/task/usecases/list-tasks.usecase.ts
@@ -1,13 +1,22 @@
+import { CacheRepository } from "../../../shared/database/repository/cache.repositiry";
 import { TaskRepository } from "../repository/task.repository";
 
 export class ListTasksUsecase {
     public async execute (userId: string, type: string) {
         const repository = new TaskRepository()
-
+        const cacheRepository = new CacheRepository()
         const typeConvert = type === 'true' ? true : false;
-
+        const cache = await cacheRepository.get(`tasks-${userId}`)
+        if(cache){
+            return {
+                ok: true,
+                code: 200,
+                message: 'Tasks successfully listed in cache',
+                date: cache
+            }
+        }
         const result = await repository.listTasks(userId, typeConvert)
-        
+        await cacheRepository.set(`tasks-${userId}`, result)        
         return result.map(task => task.toJson())
     }
 }
