faca3945f7358d48e974848f96a2817e31481c0a
AssFerj <assisjuniorcam@gmail.com>
2023-08-25 00:55:30 -0300

    feat: add logged user validator to tasks routes

--- src/app/features/task/routes/task.routes.ts
+++ src/app/features/task/routes/task.routes.ts
@@ -2,17 +2,18 @@ import { Router } from "express"
 import { TaskController } from "../controllers/tasks.controllers";
 import { createTaskValidateFields } from "../validators/createTaskValidateFields.middleware";
 import { updateTaskValidateFields } from "../validators/updateTaskValidateFields.middleware";
+import { LoginValidator } from "../validators/login.validator";
 
 export const taskRoutes = ()=> {
     const app = Router({
         mergeParams: true
     });
 
-    app.get('/', new TaskController().list);
-    app.get('/:taskId', new TaskController().getTaskById);
-    app.post('/', [createTaskValidateFields], new TaskController().create);
-    app.put('/:taskId', [/*updateTaskValidateFields*/], new TaskController().updateTask);
-    app.delete('/:taskId', new TaskController().deleteTask);
+    app.get('/', [LoginValidator.checkToken], new TaskController().list);
+    app.get('/:taskId', [LoginValidator.checkToken], new TaskController().getTaskById);
+    app.post('/', [createTaskValidateFields, LoginValidator.checkToken], new TaskController().create);
+    app.put('/:taskId', [/*updateTaskValidateFields*/LoginValidator.checkToken], new TaskController().updateTask);
+    app.delete('/:taskId', [LoginValidator.checkToken], new TaskController().deleteTask);
 
     return app;
 }
