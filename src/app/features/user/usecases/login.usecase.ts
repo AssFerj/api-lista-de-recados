f4aa085ad9be7296f4380a01fe5f4a769a918595
AssFerj <assisjuniorcam@gmail.com>
2023-08-25 00:56:42 -0300

    feat: created login usecase

--- src/app/features/user/usecases/login.usecase.ts
+++ src/app/features/user/usecases/login.usecase.ts
@@ -1,58 +1,45 @@
 import { User } from "../../../models/User";
 import { JwtService } from "../../../shared/services/jwt.service";
+import { UsecaseResponse } from "../../../shared/util/usecase.response";
 import { UserRepository } from "../repository/user.repository";
 
 interface LoginParams {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 export class LoginUsecase {
   public async execute(params: LoginParams) {
-    // buscar o user por email
     const repository = new UserRepository();
     const user = await repository.getUserByEmail(params.email);
 
     if (!user) {
-      return "User not found"
+      return UsecaseResponse.notFound('User')
     }
 
-    // if(!params.email){
-    //     return apiResponse.notProvided(res, 'E-mail');
-    // }
+    if(!params.email){
+        return UsecaseResponse.unauthorized()
+    }
 
-    // if(!params.password){
-    //     return apiResponse.notProvided(res, 'Password');
-    // }
+    if(!params.password){
+        return UsecaseResponse.unauthorized()
+    }
 
     const loggedUser = new User('', '', params.email, params.password);
 
     if(!loggedUser){
-        return {
-            ok: false,
-            message: 'Invalid credentials',
-            code: 401
-        }
+        return UsecaseResponse.unauthorized()
     }
 
     const result = await repository.login(loggedUser)
 
-    // compara as senhas
-    if (user.password !== params.password) {
-    //   return UsecaseResponse.unauthorized();
-        return {
-            ok: false,
-            message: 'Invalid credentials',
-            code: 401
-        }
+    if(user.password !== params.password) {
+        return UsecaseResponse.unauthorized()
     }
 
-    // criar um token jwt
     const token = new JwtService().createToken(user.toJson());
 
-    // retorna o user logado
-    // return UsecaseResponse.success("Login successfully done", {
-    return {
+    return UsecaseResponse.success("Login successfully done", result), {
     ...user.toJson(),
     token,
     }
