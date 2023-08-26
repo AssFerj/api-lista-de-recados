d4e6c0d783ab2f56b884ebeee636c5cae83d4ac0
AssFerj <assisjuniorcam@gmail.com>
2023-08-26 18:53:40 -0300

    refactor: refactores create task usecase to use and update cache

--- src/app/features/task/usecases/create-task.usecase.ts
+++ src/app/features/task/usecases/create-task.usecase.ts
@@ -1,16 +1,14 @@
 import { Task } from "../../../models/Task";
+import { CacheRepository } from "../../../shared/database/repository/cache.repositiry";
 import { TaskRepository } from "../repository/task.repository";
-
-interface CreateTaskByIdParams {
-    userId: string;
-    description: string;
-    archived: boolean;
-}
 
 export class CreateTaskUsecase {
     public async execute (task: Task) {
+        const cache = new CacheRepository()
         const repository = new TaskRepository();
-        
-        await repository.addTask(task)
+        const result = await repository.addTask(task)
+        await cache.delete(`task-${task.id}`)
+        await cache.delete(`tasks-${task.userId}`)
+        await cache.set(`task-${task.id}`, result)
     }
 }
