e8f1f469e720b309d3d7402ebff90048b2f8181b
AssFerj <assisjuniorcam@gmail.com>
2023-08-26 18:43:32 -0300

    refactor: refactores list tasks to use usecase responses

--- src/app/features/task/usecases/list-tasks.usecase.ts
+++ src/app/features/task/usecases/list-tasks.usecase.ts
@@ -1,28 +1,24 @@
 import { CacheRepository } from "../../../shared/database/repository/cache.repositiry";
+import { UsecaseResponse } from "../../../shared/util/usecase.response";
 import { TaskRepository } from "../repository/task.repository";
 
 export class ListTasksUsecase {
     public async execute (userId: string, type: string) {
-    
         if(!userId){
-            return 'User ID was not provided';
+            return UsecaseResponse.notFound('User ID')
         }
-        
         if(!type){
-            return 'Type was not provided';
+            return UsecaseResponse.notFound('Type')
         }
         const repository = new TaskRepository()
         const cacheRepository = new CacheRepository()
         const typeConvert = type === 'true' ? true : false;
         const cache = await cacheRepository.get(`tasks-${userId}`)
         if(cache){
-            return {
-                message: 'Tasks successfully listed in cache',
-                date: cache
-            }
+            return UsecaseResponse.success('Tasks successfully listed in cache', cache)
         }
         const result = await repository.listTasks(userId, typeConvert)
-        await cacheRepository.set(`tasks-${userId}`, result)        
+        await cacheRepository.set(`tasks-${userId}`, result.map(task => task.toJson()))        
         return result.map(task => task.toJson())
     }
 }
