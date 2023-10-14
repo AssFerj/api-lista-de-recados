6b2a7c97870a232ff977fabe12b2d1c56ca85468
AssFerj <assisjuniorcam@gmail.com>
2023-10-14 20:14:57 -0300

    feat: created migration

--- src/app/shared/database/migrations/1692560094005-criar-tabelas.ts
+++ src/app/shared/database/migrations/1692560094005-criar-tabelas.ts
@@ -4,15 +4,15 @@ export class criarTabelas1692560094005 implements MigrationInterface {
     name = 'criarTabelas1692560094005'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "lista_recados"."users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "first_name" character varying(30) NOT NULL, "last_name" character varying(30) NOT NULL, "email" character varying(50) NOT NULL, "password" character varying(30) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "lista_recados"."tasks" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "user_id" uuid NOT NULL, "description" character varying(100) NOT NULL, "archived" boolean NOT NULL DEFAULT false, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_8d12ff38fcc62aaba2cab748772" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "lista_recados"."tasks" ADD CONSTRAINT "FK_db55af84c226af9dce09487b61b" FOREIGN KEY ("user_id") REFERENCES "lista_recados"."users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`CREATE TABLE "lista-recados"."users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "first_name" character varying(30) NOT NULL, "last_name" character varying(30) NOT NULL, "email" character varying(50) NOT NULL, "password" character varying(30) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "lista-recados"."tasks" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "user_id" uuid NOT NULL, "description" character varying(100) NOT NULL, "archived" boolean NOT NULL DEFAULT false, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_8d12ff38fcc62aaba2cab748772" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "lista-recados"."tasks" ADD CONSTRAINT "FK_db55af84c226af9dce09487b61b" FOREIGN KEY ("user_id") REFERENCES "lista-recados"."users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "lista_recados"."tasks" DROP CONSTRAINT "FK_db55af84c226af9dce09487b61b"`);
-        await queryRunner.query(`DROP TABLE "lista_recados"."tasks"`);
-        await queryRunner.query(`DROP TABLE "lista_recados"."users"`);
+        await queryRunner.query(`ALTER TABLE "lista-recados"."tasks" DROP CONSTRAINT "FK_db55af84c226af9dce09487b61b"`);
+        await queryRunner.query(`DROP TABLE "lista-recados"."tasks"`);
+        await queryRunner.query(`DROP TABLE "lista-recados"."users"`);
     }
 
 }
